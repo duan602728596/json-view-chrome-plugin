855553517e71abb56fac5c7a40959d50381a682f
段昊辰 <duanhaochen@126.com>
2019-12-04 09:41:15 +0800

    build: 调整babel的配置

--- babel.config.js
+++ babel.config.js
@@ -2,21 +2,6 @@
 const babelConfig = {
   presets: [
     '@babel/preset-react'
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true
-      }
-    ],
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-do-expressions',
-    '@babel/plugin-proposal-optional-catch-binding',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-syntax-bigint'
   ]
 };
 
